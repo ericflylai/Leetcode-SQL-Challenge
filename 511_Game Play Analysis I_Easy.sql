65c27740bbc47afd5e8e01bab0ee53002591e2d9
Eric Lai <eric.laihf@gmail.com>
2022-01-29 23:18:11 -0500

    Update 511_Game Play Analysis I_Easy.sql

--- 511_Game Play Analysis I_Easy.sql
+++ 511_Game Play Analysis I_Easy.sql
@@ -1,6 +1,7 @@
-#511. Game Play Analysis I
+#Write an SQL query to report the first login date for each player.
 
-#Solution
+#Return the result table in any order.
+
 select distinct player_id, min(event_date) as first_login
 from Activity
 group by player_id
