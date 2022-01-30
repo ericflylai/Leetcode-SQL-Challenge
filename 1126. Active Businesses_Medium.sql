c5f4a99ee807dcdb248b96fdd6a8870c82e2fc09
Eric Lai <eric.laihf@gmail.com>
2022-01-30 14:27:47 -0500

    Update 1126. Active Businesses_Medium.sql

--- 1126. Active Businesses_Medium.sql
+++ 1126. Active Businesses_Medium.sql
@@ -1,30 +1,24 @@
-#Write an SQL query to find the average daily percentage of posts that got removed after being reported as spam, rounded to 2 decimal places.
+#The average activity for a particular event_type is the average occurences across all companies that have this event.
+
+#An active business is a business that has more than one event_type such that their occurences is strictly greater than the average activity for that event.
+
+#Write an SQL query to find all active businesses.
 
 
-#Thought: select all report & spam records, mark as 1
-# then join removals table to mark post_id which has been removed
-# it mentioned post_id is PK in second table, so no need to worry about some post_id and different remove_date situation. 
-# Last, calculate the result.
-with 
+with
 a as (
-select post_id,action_date,mark,case
-when remove_date is not null then 1
-else 0
-end as remove_mark
-from
-# read question carefully, in Actions table, it mentioned there is no primary key for this table, it may have duplicate rows. Thus,we need to select distinct one and exclude duplicated rows.Otherwise, it won't be passed by all tests
-(select distinct post_id,action_date,case
-when action = 'report' and extra ='spam' then 1
+# mark all rows meet requirement as 1
+select distinct business_id,event_type,case
+when t.occurences > t.avg_occurences then 1
 else 0
 end as mark
-from Actions) as a
-left join Removals
-using (post_id)
-where mark > 0
-)
-
-select round(avg(remove_mark1/mark1*100),2) as average_daily_percent
-from(
-select distinct action_date,sum(remove_mark) as remove_mark1,sum(mark) as mark1
+from (
+# first calculate each event type avg occurences
+select business_id,event_type,occurences,avg(occurences) over (partition by event_type) as avg_occurences
+from events) as t
+    )
+#select sum of mark > 1 from a
+select distinct business_id
 from a
-group by action_date) as b
+group by business_id
+having sum(mark)>1
