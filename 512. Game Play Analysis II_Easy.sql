c2cc6bce9b5082f5b42bb93a0c98ff751d245668
Eric Lai <eric.laihf@gmail.com>
2022-01-29 23:16:42 -0500

    Create 512. Game Play Analysis II_Easy.sql

---
+++ 512. Game Play Analysis II_Easy.sql
@@ -0,0 +1,10 @@
+with 
+a1 as (
+select distinct player_id, min(event_date) as firstlog
+from Activity
+group by player_id)
+
+select a1.player_id,b1.device_id
+from a1 
+left join Activity b1
+on a1.player_id=b1.player_id and a1.firstlog=b1.event_date
