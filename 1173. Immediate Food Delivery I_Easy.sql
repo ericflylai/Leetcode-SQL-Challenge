965acb4b89e4b8e00071ee05b7415ac487cf3725
Eric Lai <eric.laihf@gmail.com>
2022-02-01 18:25:32 -0500

    Update 1173. Immediate Food Delivery I_Easy.sql

--- 1173. Immediate Food Delivery I_Easy.sql
+++ 1173. Immediate Food Delivery I_Easy.sql
@@ -1,6 +1,6 @@
-If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+#If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
 
-Write an SQL query to find the percentage of immediate orders in the table, rounded to 2 decimal places.
+#Write an SQL query to find the percentage of immediate orders in the table, rounded to 2 decimal places.
 
 #question related to calculate percentage, first thing comes up in my mind is use "case when" to mark rows
 
