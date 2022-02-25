5953571e434e08f535e91e90a0a2d51b55301107
Eric Lai <eric.laihf@gmail.com>
2022-02-24 22:46:00 -0500

    Create 1341. Movie Rating_Medium.sql

---
+++ 1341. Movie Rating_Medium.sql
@@ -0,0 +1,25 @@
+#Write an SQL query to:
+
+#Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+#Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+
+#using union all function
+
+(select name as results
+from MovieRating
+left join Users
+using(user_id)
+group by user_id
+order by count(movie_id) desc, name
+limit 1 )
+
+union all
+
+(select title as results
+from MovieRating
+left join Movies
+using (movie_id)
+where created_at between '2020-02-01' and '2020-2-28'
+group by movie_id
+order by avg(rating) desc, title
+limit 1)
